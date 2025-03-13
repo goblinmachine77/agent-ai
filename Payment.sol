9d4f18002d57aef4f8aebb76da11c1005027d101
goblinmachine77 <goblinmachine77@gmail.com>
2025-03-13 15:26:00 +0530

    Update Payment.sol

--- Payment.sol
+++ Payment.sol
@@ -1,12 +1,8 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
-
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/interfaces/IERC721.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
 using SafeERC20 for IERC20;
-
 contract Payment {
     function checkNFTOwnership(address nftTokenAddress, uint256 nftId, address userAddress)
         public
@@ -19,9 +15,6 @@ contract Payment {
         IERC721 nftToken = IERC721(nftTokenAddress);
         return nftToken.ownerOf(nftId) == userAddress;
     }
-
-    // This is to support gasless flow: normally, the caller must always be the msg.sender
-    // slither-disable-next-line arbitrary-send-erc20
     function payWithERC20(address erc20TokenAddress, uint256 amount, address fromAddress, address toAddress) public {
         // check from and to address
         require(fromAddress != toAddress, "Cannot transfer to self address");
